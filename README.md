47695e1f045800e9cdc3bd3d42b13bded0cbe1ed
ILLDOM <illi.dominique.v@gmail.com>
2022-11-09 15:51:37 +0100

    updated README, fixed help lines, skaffold push command

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GPS injector
+This cli tool can:
+    - download the ls_node collection and add coordinated fields. The output is saved in the specified file. ``gps-injector pull -o <file.json>``
+    - upload the coordinates into a new collection called ls_node_coordinates
+
+## adjust jalapeno parameter
+update the environment file to match your Jalapeno ArangoDB connection parameters
--- cmd/push.go
+++ cmd/push.go
@@ -1,1 +1,20 @@
 package cmd
+
+import "github.com/spf13/cobra"
+
+var input string // flag to specify input string
+
+func NewPushCmd() *cobra.Command {
+	pushCmd := &cobra.Command{
+		Use:     "push",
+		Short:   "push and save nodes with coordindates from input file into arango ls_node_coordinates collection",
+		Example: "gps-injector push -i coordinates.json",
+
+		Run: func(cmd *cobra.Command, args []string) {
+
+		},
+	}
+	pushCmd.Flags().StringVar(&input, "i", "coordinates.json", "Flag to specify input file")
+	pushCmd.MarkFlagRequired("i")
+	return pushCmd
+}
